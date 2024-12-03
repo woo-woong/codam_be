3142665d8aca7c1c04ed3d03cb07d51c9286c6f8
권상웅 <kw3082@naver.com>
2024-12-03 18:46:59 +0900

    🚀 initialize

---
+++ src/test/java/com/example/miraclediscord/MiracleDiscordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miraclediscord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiracleDiscordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
